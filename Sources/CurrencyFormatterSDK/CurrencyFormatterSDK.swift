d4ca51172d0d9ddb73091fc4587ee8d8001749ee
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2024-03-18 23:59:37 +0300

    initial commit

---
+++ Sources/CurrencyFormatterSDK/CurrencyFormatterSDK.swift
@@ -0,0 +1,25 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+public final class CurrencyFormatter {
+    
+    private let formatter: NumberFormatter
+    
+    init(formatter: NumberFormatter = defaultFormatter()) {
+        self.formatter = formatter
+    }
+    
+    /// Number Style = decimal
+    public static func defaultFormatter() -> NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter
+    }
+    
+    public func format(intValue: Int) {
+        print("formatting int value :)")
+    }
+    
+}
